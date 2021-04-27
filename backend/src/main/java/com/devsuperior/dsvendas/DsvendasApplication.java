793244e6db0a2d2ab69ebd2f005ec613c9807303
Nelio Alves <acenelio@gmail.com>
2021-04-26 22:49:01 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
